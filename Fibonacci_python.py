5241f54eac989eef6381344ee1b16a6a0472fcfb
Kobumanzi Trishia <kobu2rsha@gmail.com>
2024-12-10 14:40:32 -0800

    Add files via upload

---
+++ Fibonacci_python.py
@@ -0,0 +1,30 @@
+#Fibonacci using for loop
+prev2 = 0
+prev1 = 1
+
+print(prev2)
+print(prev1)
+for fibo in range(18):
+    newFibo = prev1 + prev2
+    print(newFibo)
+    prev2 = prev1
+    prev1 = newFibo
+
+#Fibonacci using recursion
+print(0)
+print(1)
+count = 2
+
+def fibonacci(prev1, prev2):
+    global count
+    if count <= 19:
+        newFibo = prev1 + prev2
+        print(newFibo)
+        prev2 = prev1
+        prev1 = newFibo
+        count += 1
+        fibonacci(prev1, prev2)
+    else:
+        return
+
+fibonacci(1,0)
